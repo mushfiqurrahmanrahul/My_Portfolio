4b7ad1822a95d4632da1a5521de479d5c03931b7
Md.Mushfiqur Rahman Rahul <mushfiqrahul2000@gmail.com>
2026-01-07 01:40:10 +0600

    Update form submission to include email subject
    Set email subject based on user input before form submission.

--- js/main.js
+++ js/main.js
@@ -277,6 +277,8 @@ if(form){  //  safety check
   form.addEventListener("submit", async function (e) {
     e.preventDefault();
 
+	  document.getElementById("emailSubject").value = "Portfolio Contact: " + document.querySelector('input[name="subject"]').value;
+
     const formData = new FormData(form);
 
     try {
